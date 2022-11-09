3b9b9fcb51ff33de6c5d975d703b6ec83ac9a7ad
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2022-11-09 14:59:59 +0900

    ローカル側で編集

--- PM1_Git.cpp
+++ PM1_Git.cpp
@@ -5,11 +5,10 @@
 
 int main()
 {
-    std::cout << "Hello GitHub World\n";
-    
-    std::cout << "New branch　changed\n";
+    std::cout << "Hello Local_GitHubWorld \n";
 
-    std::cout << "third branch\n";
+    std::cout << "Hello Local_ConflictBranch! \n";
+    
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
